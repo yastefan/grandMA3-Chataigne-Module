ab01c56752bc34d264d79124ad20f2864a451c2f
yastefan <stefanschmidt@posteo.org>
2022-05-01 12:37:06 +0200

    add offset feature

--- grandMA3.js
+++ grandMA3.js
@@ -25,22 +25,26 @@ function moduleValueChanged(value) {
   }
 }
 
-function moveExecutorFader(page, executor, value) {
+function moveExecutorFader(page, executor, offset, value) {
   var range = local.parameters.faderRange.get();
+  executor = executor + offset;
   local.send("/Page" + page + "/Fader" + executor + "/", value*range);
 }
 
-function pushExecutorButton(page, executor, value) {
+function pushExecutorButton(page, executor, offset, value) {
   if(value) value = 1;
+  executor = executor + offset;
   local.send("/Page" + page + "/Key" + executor + "/", value);
 }
 
-function moveSequenceFader(sequenceNumber, fader, value) {
+function moveSequenceFader(sequenceNumber, fader, offset, value) {
   var range = local.parameters.faderRange.get();
+  sequenceNumber = sequenceNumber + offset;
   local.send("/13.13.1.5." + sequenceNumber, fader, 1, value*range);
 }
 
 function pushSequenceButton(sequenceNumber, button, value) {
+  sequenceNumber = sequenceNumber + offset;
   if(value) value = 1;
   local.send("/13.13.1.5." + sequenceNumber, button, value);
 }
