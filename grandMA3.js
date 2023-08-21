e6141ed6bcc0bea15c27034ff6dd1e31bb61f54e
7heMech <83923848+7heMech@users.noreply.github.com>
2023-08-22 01:02:35 +0300

    Update grandMA3.js
    Removed a lot of repetition, and used some modern js.
    Hopefully cleaned up the code.
    Check it out and tell me what ya think.

--- grandMA3.js
+++ grandMA3.js
@@ -1,145 +1,93 @@
-var sequenceContainer = {};
+const sequenceContainer = {};
 
 function init() {
-  script.log("grandMA3 module loaded");
+  script.log('grandMA3 module loaded');
 }
 
-function moduleParameterChanged(param) {
-  script.log(param.name + " parameter changed, new value: " + param.get());
-}
-
-function moduleValueChanged(value) {
-  if(value.name == "preview") {
-    if(value.get()) {
-      local.send("/cmd", "preview on");
-    }
-    else {
-      local.send("/cmd", "preview off");
-    }
-  }
-  else if(value.name == "blind") {
-    if(value.get()) {
-      local.send("/cmd", "blind on");
-    }
-    else {
-      local.send("/cmd", "blind off");
-    }
-  }
-  else if(value.name == "freeze") {
-    if(value.get()) {
-      local.send("/cmd", "freeze on");
-    }
-    else {
-      local.send("/cmd", "freeze off");
-    }
-  }
-}
-
-function moveExecutorFader(page, executor, offset, value) {
-  var range = local.parameters.faderRange.get();
-  executor = executor + offset;
-  if(page == 0)
-  {
-    local.send("/Fader" + executor + "/", value*range);
-  }
-  else
-  {
-    local.send("/Page" + page + "/Fader" + executor + "/", value*range);
-  }
-}
-
-function pushExecutorButton(page, executor, offset, value) {
-  if(value) value = 1;
-  executor = executor + offset;
-  if(page == 0)
-  {
-    local.send("/Key" + executor + "/", value);
-  }
-  else
-  {
-    local.send("/Page" + page + "/Key" + executor + "/", value);
-  }
-}
-
-function turnExecutorEncoder(page, executor, offset, multiplicator) {
-  executor = executor + offset;
-  if(page == 0)
-  {
-    local.send("/Encoder" + executor + "/", multiplicator);
-  }
-  else
-  {
-    local.send("/Page" + page + "/Encoder" + executor + "/", multiplicator);
-  }
-}
-
-function moveSequenceFader(sequenceNumber, offset, fader, value) {
-  var range = local.parameters.faderRange.get();
-  sequenceNumber = sequenceNumber + offset;
-  local.send("/13.13.1.6." + sequenceNumber, fader, 1, value*range);
-}
-
-function pushSequenceButton(sequenceNumber, offset, button, value) {
-  sequenceNumber = sequenceNumber + offset;
-  if(value) value = 1;
-  local.send("/13.13.1.6." + sequenceNumber, button, value);
-}
-
-function moveGrandMasterFader(grandMaster, value) {
-  var range = local.parameters.faderRange.get();
-  local.send("/13.12.2." + grandMaster, "FaderMaster", 1, value*range);
-}
-
-function moveGrandMasterBpmFader(grandMaster, value) {
-  var range = local.parameters.faderRange.get();
-  local.send("/13.12.2." + grandMaster, "FaderMaster", 1, Math.sqrt(value/240)*range);
-}
-
-function moveSpeedMasterFader(speedMaster, value) {
-  var range = local.parameters.faderRange.get();
-  local.send("/13.12.3." + speedMaster, "FaderMaster", 1, value*range);
-}
-
-function moveSpeedMasterBpmFader(speedMaster, value) {
-  var range = local.parameters.faderRange.get();
-  local.send("/13.12.3." + speedMaster, "FaderMaster", 1, Math.sqrt(value/240)*range);
-}
-
-function turnEncoder(encoder, multiplicator, value) {
-  script.log("Attribute " + encoder + " at + " + value*multiplicator);
-  local.send("/cmd", "Attribute " + encoder + " at + " + value*multiplicator);
+function moduleParameterChanged({ get, name }) {
+  script.log(`${name} parameter changed, new value: ${get()}`);
 }
 
 function sendCommand(command) {
-  local.send("/cmd", command);
+  local.send('/cmd', command);
 }
 
-function setPreview(onStatus) {
-  local.values.preview.set(onStatus);
-  if(onStatus) {
-    local.send("/cmd", "preview on");
-  }
-  else {
-    local.send("/cmd", "preview off");
-  }
+function getRange() {
+  return local.parameters.faderRange.get();
 }
 
-function setBlind(onStatus) {
-  local.values.blind.set(onStatus);
-  if(onStatus) {
-    local.send("/cmd", "blind on");
-  }
-  else {
-    local.send("/cmd", "blind off");
-  }
+function moduleValueChanged({ get, name }) {
+  sendCommand(`${name} ${get() ? 'on' : 'off'}`);
 }
 
-function setFreeze(onStatus) {
-  local.values.freeze.set(onStatus);
-  if(onStatus) {
-    local.send("/cmd", "freeze on");
-  }
-  else {
-    local.send("/cmd", "freeze off");
-  }
+function sendControlMessage(type, page, executor, offset, value) {
+  executor += offset;
+  const prefix = page === 0 ? '' : `/Page${page}`;
+
+  if (type == 'Key') value &&= 1;
+  if (type == 'Fader') value *= getRange();
+  
+  local.send(`${prefix}/${type}${executor}/`, value);
+}
+
+function moveExecutorFader(...args) {
+  sendControlMessage('Fader', ...args);
+}
+
+function pushExecutorButton(...args) {
+  sendControlMessage('Key', ...args);
+}
+
+function turnExecutorEncoder(...args) {
+  sendControlMessage('Encoder', ...args);
+}
+
+function moveSequenceFader(sequenceNumber, offset, fader, value) {
+  sequenceNumber += offset;
+  local.send(`/13.13.1.6.${sequenceNumber}`, fader, 1, value * getRange());
+}
+
+function pushSequenceButton(sequenceNumber, offset, button, value) {
+  sequenceNumber += offset;
+  value &&= 1;
+  local.send(`/13.13.1.6.${sequenceNumber}`, button, value);
+}
+
+function moveGrandMasterFader(grandMaster, value) {
+  local.send(`/13.12.2.${grandMaster}`, "FaderMaster", 1, value * getRange());
+}
+
+function moveGrandMasterBpmFader(grandMaster, value) {
+  local.send(`/13.12.2.${grandMaster}`, "FaderMaster", 1, Math.sqrt(value / 240) * getRange());
+}
+
+function moveSpeedMasterFader(speedMaster, value) {
+  local.send(`/13.12.3.${grandMaster}`, "FaderMaster", 1, value * getRange());
+}
+
+function moveSpeedMasterBpmFader(speedMaster, value) {
+  local.send(`/13.12.3.${grandMaster}`, "FaderMaster", 1, Math.sqrt(value / 240) * getRange());
+}
+
+function turnEncoder(encoder, multiplicator, value) {
+  const at = value * multiplicator;
+  script.log(`Attribute ${encoder} at ${at}`);
+  sendCommand(`Attribute ${encoder} at ${at}`);
+}
+
+function set(name, status) {
+  local.values[name].set(status);
+  sendCommand(`${name} ${status ? 'on' : 'off'}`);
+}
+
+function setPreview(status) {
+  set('preview', status);
+}
+
+function setBlind(status) {
+  set('blind', status);
+}
+
+function setFreeze(status) {
+  set('freeze', status);
 }
